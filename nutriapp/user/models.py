daf24ec782b59ebead9bec72c28fe0b325d59fce
raulieair <168190752+raulieair@users.noreply.github.com>
2024-06-06 11:06:32 +0200

    Update models.py

--- nutriapp/user/models.py
+++ nutriapp/user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -11,17 +12,23 @@ class User(AbstractUser):
 
 
 class Patient(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='patient')
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='patient',null=True,blank=True)
     peso = models.IntegerField(null=True,blank=True)
     altura = models.IntegerField(null=True,blank=True)
     fecha_nacimiento = models.DateField(null=True,blank=True)
 
+    def __str__(self):
+        return self.user.username
+@receiver(post_save,sender=User)
 def create_patient(sender, instance, created, **kwargs):
-    if User.is_patient ==True:
+    if created and instance.is_patient ==True:
         Patient.objects.create(user=instance)
+        instance.patient.save()
+    else:
+        return
+#@receiver(post_save,sender=User)
+#def save_user_patient(sender,instance,**kwargs):
+    
 
-def save_user_patient(sender,instance,**kwargs):
-    instance.patient.save()
-
-post_save.connect(create_patient, sender=User)
-post_save.connect(save_user_patient,sender=User)
+#post_save.connect(create_patient, sender=User)
+#post_save.connect(save_user_patient,sender=User)
